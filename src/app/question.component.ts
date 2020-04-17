89a2c62cc83835f1a56069203e688d8331c4f284
Yi HAN <hanyi@8110@gmail.com>
2020-04-17 14:24:09 +0200

    POST a question containing quizId

--- src/app/question.component.ts
+++ src/app/question.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { ApiService } from './api.service'
+import { ActivatedRoute }  from '@angular/router'
 
 @Component({
     selector: 'question',
@@ -9,13 +10,18 @@ export class QuestionComponent {
 
     question : any = {}
 
-    constructor(public api: ApiService){}
+    quizId: number 
+
+    constructor(public api: ApiService, private route: ActivatedRoute){}
 
     ngOnInit(){
+        this.quizId =   Number.parseInt(this.route.snapshot.paramMap.get('quizId'))
+        console.log(this.quizId)
         this.api.questionSelected.subscribe(q => this.question = q)
     }
 
     create(question) {
+        question.quizId = this.quizId
         this.api.createQuestion(question)
     }
 
